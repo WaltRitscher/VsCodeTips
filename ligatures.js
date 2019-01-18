fc8bfe1f0d7122798042b61cb56ea5f79e19f11f
Walt Ritscher <waltritscher@outlook.com>
2019-01-17 20:58:40 -0800

    First code samples added

---
+++ ligatures.js
@@ -0,0 +1,27 @@
+// Fira Code: monospaced font with programming ligatures
+
+ // From the Fira Code developer
+ // Programmers use a lot of symbols, often encoded with several characters. Ideally, all programming languages should be designed with full-fledged Unicode symbols for operators, but that’s not the case yet.
+
+UseFontLigatures()
+{
+ // Fira Code is based on Fira Mono
+ // nice contrast on ambigous charactors
+
+ // I vs l vs | vs 1, O vs 0
+ // braces []{}()
+
+  var x = 10;
+
+  // ==, === => -> :=
+  if (x==20)
+  {
+    
+
+    if (x===30){;}
+    x++ ;
+    if (x!=40){;}
+
+  
+  }
+}
