1f8b33de52b7de5c59a66d1d425b93d566ed661b
Walt Ritscher <waltritscher@outlook.com>
2019-01-17 22:04:27 -0800

    Add spacing

--- ligatures.js
+++ ligatures.js
@@ -2,7 +2,7 @@
 
  // From the Fira Code developer
  // Programmers use a lot of symbols, often encoded with several characters. Ideally, all programming languages should be designed with full-fledged Unicode symbols for operators, but that’s not the case yet.
-
+ 
 UseFontLigatures()
 {
  // Fira Code is based on Fira Mono
