4520957f50a1a5c124b448377a63e8d320e77ac0
Walt Ritscher <waltritscher@outlook.com>
2019-01-18 21:00:52 -0800

    Add Color Brace demo

---
+++ BraceColors.js
@@ -0,0 +1,18 @@
+function LiveDemo() {
+  var wholesaleCost = 45.89;
+
+  var markup = .30;
+  var retail = wholesaleCost * markup;
+  var oddNumbers = new [1, 3, 5, 7];
+ 
+  if (markup > 5. && wholesaleCost > 2000) {
+      if (((markup + 2) % 10) == 0) {
+      {
+
+      }
+
+    }
+
+  }
+
+}
